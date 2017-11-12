264b6b394f6b4302692d401c16fb1aca7289446d
Rahul Raj <rahulrajj2009@gmail.com>
2017-11-12 19:19:49 +0530

    chatbot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
+interface Post {
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  chats: FirebaseListObservable<any[]>;
+  
+  constructor(public af: AngularFireDatabase) {
+    this.chats = af.list('chats');
+    
+  }
+
+  botAI:{index: number,id: string, response: string, contents: any}[] = [
+    {index: 1,id: 'Welcome',response: 'Hi, Welcome to ChefRef', contents: ['HI','HELLO','HOLA','HAI']},
+    {index:2,id: 'Error',response: 'Hi, I am sorry. I am still in the learning phase', contents: []},
+  ];
+  messageInput: string;
+  responseMsg: string;
+  sendMessage(){
+    let message = {
+      status: true,
+      message: this.messageInput,
+      date: new Date().toString()
+    }
+    this.chats.push(message);
+    
+    setTimeout(()=>{   
+          this.checkResponse(this.messageInput);
+          this.messageInput = '';
+    },1000);
+    
+  }
+
+  checkResponse(msg){
+    
+    for(let i=0; i< this.botAI.length; i++){
+      if(this.botAI[i].contents.includes(msg.toUpperCase())){
+        this.responseMsg = this.botAI[i].response;
+        break;
+      }else{
+        this.responseMsg = 'Hi, I am sorry. I am still in the learning phase';
+        break;
+      }
+    }
+    let response = {
+      status: false,
+      message: this.responseMsg,
+      date: new Date().toString()
+    }
+    this.chats.push(response);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database-deprecated';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),  
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
